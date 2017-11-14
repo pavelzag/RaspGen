b0a1a42ff3b21cec75761f6226ad2aa0091dd8eb
pavelzagalsky <zagalsky@gmail.com>
2017-11-14 20:16:08 +0200

    Fixed permanent going down message

--- send_mail.py
+++ send_mail.py
@@ -1,14 +1,12 @@
 import smtplib
 from configuration import get_config
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-sender_email = get_config('email')
-sender_user = get_config('user')
-sender_password = get_config('password')
-server.login(sender_email, sender_password)
-
 
 def send_mail(recipient='zagalsky@gmail.com', msg="Hi"):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    sender_email = get_config('email')
+    sender_password = get_config('password')
+    server.login(sender_email, sender_password)
     server.sendmail(sender_email, recipient, msg)
